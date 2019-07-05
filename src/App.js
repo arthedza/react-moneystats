845bf4c5ea2420657e38fb3c25cd89af3f0ea88d
Artur Zahorskyi <arthedza@gmail.com>
2019-07-06 00:15:07 +0300

    UI

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import './App.css';
 import wallet from './wallet.svg'
-import { Menu, Icon, Affix, Button } from 'antd';
+import { Menu, Icon, Affix, Button, Layout, List } from 'antd';
+import {Router, Route, Redirect, Link, Switch} from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+// import createHistory from "history/createBrowserHistory";
+
+const {Header, Sider, Content} = Layout;
+
 
 const Sidebar = p => (
   <div className="Sidebar">
@@ -9,14 +15,17 @@ const Sidebar = p => (
     <img src={wallet} className="App-logo" alt="logo" />
       MoneyStats
     </div>
+    <div className="SideMenu">
     <SideMenu/>
+    </div>
+    
     <span id="copyright">Copyright © 2019, Artedza</span>
   </div>
   
 )
 
 const SideMenu = p => (
-  <div className="SideMenu">
+  <div >
     
     <Menu 
       defaultSelectedKeys={['1']}
@@ -27,26 +36,30 @@ const SideMenu = p => (
       <Menu.Item key="1">
         <Icon type="container" />
         <span>Журнал</span>
+        <Link to='/journal'></Link>
       </Menu.Item>
       <Menu.Item key="2">
         <Icon type="credit-card" />
         <span>Счета</span>
+        <Link to='/accounts'></Link>
       </Menu.Item>
       <Menu.Item key="3">
         <Icon type="fall" />
         <span>Расходы</span>
+        <Link to='/spending'></Link>
       </Menu.Item>
       <Menu.Item key="4">
         <Icon type="rise" />
         <span>Доходы</span>
+        <Link to='/income'></Link>
       </Menu.Item>
     </Menu>
   </div>
 )
 
 const Topbar = p => (
-  <div className="Topbar">
-  dgjsfslkfjshf
+  <div className="Title">
+  Title
   </div>
 )
 
@@ -66,13 +79,84 @@ const AffixButton = p => (
   </Affix>
 )
 
+const Journal = function(p) {
+  const data = [
+    {
+      title: 'Продукты и бакалея',
+    },
+    {
+      title: 'Обеды',
+    },
+    {
+      title: 'Транспортные карты',
+    },
+    {
+      title: 'Кофе',
+    },
+  ];
+  return (
+    <div >
+      Journal
+      <div id="date">25 мая, суббота</div>
+      <List id="transactions"
+      itemLayout="horizontal"
+      dataSource={data}
+      renderItem={item => (
+        <List.Item>
+          <List.Item.Meta
+            /*avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}*/
+            title={<a href="https://ant.design">{item.title}</a>}
+            description="Наличные"
+          /> <div id="total">-45</div>
+        </List.Item>
+      )}>
+        
+      </List>
+    </div>
+  )
+} 
+
+const Accounts = p => (
+  <div >Accounts</div>
+)
+
+const Spending = p => (
+  <div >Spending</div>
+)
+
+const Income = p => (
+  <div >Income</div>
+)
+
 function App() {
   return (
-    <div className="App">
-      <Sidebar/>
-      <Topbar/>
-      <AffixButton/>
-    </div>
+    <Router history = {createBrowserHistory()}>
+      <Layout>
+        {/* <div className="App"> */}
+        {/* <AffixButton/> */}
+        <Sider>
+        <Sidebar/>
+        </Sider>
+        
+        <div className="Wrapper">
+        <Header>
+        <Topbar/>
+        </Header>
+        
+        <Content>
+        <Redirect from="/" to="/journal" />
+          <Route path="/journal" component = {Journal} />
+          <Route path="/accounts" component = {Accounts} />
+          <Route path="/spending" component = {Spending} />
+          <Route path="/income" component = {Income} />
+        </Content>
+          
+        </div>
+        
+        
+        {/* </div> */}
+      </Layout>
+    </Router>  
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+// import {Router} from 'react-router-dom';
+// import { createBrowserHistory } from 'history'
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
